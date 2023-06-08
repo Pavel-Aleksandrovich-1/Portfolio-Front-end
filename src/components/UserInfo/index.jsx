8d03bb1661bb8212bbf0bf3bb9616d735cbc7610
Pavel-Aleksandrovich-1 <pavelaleksandrovichgid@gmail.com>
2023-06-08 13:43:04 +0700

    color

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -5,7 +5,6 @@ export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
   return (
     <div className={styles.root}>
       <div className={styles.userDetails}>
-        <span className={styles.userName}>{fullName}</span>
       </div>
     </div>
   );
