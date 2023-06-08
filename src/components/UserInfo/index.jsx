597cc0a8977ecfd95b95cd3f6685338c2d4ff2ad
Pavel-Aleksandrovich-1 <pavelaleksandrovichgid@gmail.com>
2023-06-08 13:36:45 +0700

    userinfo

--- src/components/UserInfo/index.jsx
+++ src/components/UserInfo/index.jsx
@@ -4,10 +4,8 @@ import styles from './UserInfo.module.scss';
 export const UserInfo = ({ avatarUrl, fullName, additionalText }) => {
   return (
     <div className={styles.root}>
-      <img className={styles.avatar} src={avatarUrl || '/public/favicon.ico'} alt={fullName} />
       <div className={styles.userDetails}>
         <span className={styles.userName}>{fullName}</span>
-        <span className={styles.additional}>{additionalText}</span>
       </div>
     </div>
   );
